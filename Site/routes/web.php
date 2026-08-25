4f7b505078981abe14fa57025138ae7d478d0f2d
antikwar89 <97385150+ADIPISKAR@users.noreply.github.com>
2026-08-25 22:34:14 +0300

    SEO

--- Site/routes/web.php
+++ Site/routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/sitemap.xml', function () {
+    return response()->view('sitemap')->header('Content-Type', 'application/xml');
+});
+
+Route::get('/robots.txt', function () {
+    return response("User-agent: *\nDisallow:\nSitemap: " . url('/sitemap.xml') . "\n")
+        ->header('Content-Type', 'text/plain');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
